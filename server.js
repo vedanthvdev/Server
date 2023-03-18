097db598ed5f66d2608a71865f53c1207f617bfc
VedanthVDev <vedanthvdev1996@gmail.com>
2023-03-18 19:14:46 +0000

    encryption Added

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
-// const https = require("https");
-// const fs = require("fs");
+const bcrypt = require("bcrypt");
 const PORT = 3000;
 
 const app = express();
@@ -23,17 +22,23 @@ app.use(cors());
 const db = mysql.createConnection({
   user: "root",
   host: "hospital-3.c8yzldilma0u.ap-southeast-1.rds.amazonaws.com",
-  // host: "89.242.191.214",
+  // host: "localhost",
   password: "password",
   database: "LoginSystem",
 });
 
+// Encrypting the password
+async function hashPassword(password) {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+}
+
 // Register user into the database
-app.post("/api/signup", (req, res) => {
+app.post("/api/signup", async (req, res) => {
   const firstname = req.body.firstname;
   const lastname = req.body.lastname;
   const email = req.body.email;
-  const password = req.body.password;
+  const password = await hashPassword(req.body.password);
   const gender = req.body.gender;
   const dob = req.body.dob;
 
@@ -83,17 +88,21 @@ app.post("/api/authenticate", (req, res) => {
   const password = req.body.password;
 
   db.query(
-    "SELECT * FROM users WHERE u_email = ? AND u_password = ?",
-    [email, password],
-    (err, result) => {
+    "SELECT * FROM users WHERE u_email = ?",
+    [email],
+    async (err, result) => {
       if (err) {
         res.send({ err: err });
       }
 
       if (result.length > 0) {
-        res.send(result);
+        if (await bcrypt.compare(password, result[0].u_password)) {
+          res.send(result);
+        } else {
+          res.send({ message: "Wrong email/password" });
+        }
       } else {
-        res.send({ message: "Wrong username/password" });
+        res.send({ message: "Email doesn't exist.." });
       }
     }
   );
@@ -148,6 +157,28 @@ app.post("/api/forgotpassword", (req, res) => {
       res.send({ message: "Email not found" });
     }
   });
+});
+
+// update password
+app.post("/api/updatepassword", async (req, res) => {
+  const password = await hashPassword(req.body.password);
+  const id = req.body.id;
+
+  db.query(
+    "UPDATE `LoginSystem`.`users` SET `u_password` = ? WHERE (`u_id` = ?);",
+    [password, id],
+    (err, result) => {
+      if (err) {
+        res.send({ err: err });
+      }
+
+      if (result.length > 0) {
+        res.send(result);
+      } else {
+        res.send({ message: "Email not found" });
+      }
+    }
+  );
 });
 
 //Get all user Uploaded jobs
@@ -224,15 +255,6 @@ app.get("/api/getrecentjobs", (req, res) => {
     }
   );
 });
-
-// Configure HTTPS with SSL/TLS certificates
-// const httpsOptions = {
-//   cert: fs.readFileSync("../server/cert.pem"),
-//   key: fs.readFileSync("../server/key.pem"),
-// };
-// https.createServer(httpsOptions, app).listen(PORT, () => {
-//   console.log("running server on port - " + PORT);
-// });
 
 app.listen(PORT, () => {
   console.log("running server on port - " + PORT);

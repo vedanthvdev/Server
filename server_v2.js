c1cd515c0d0f288dcf1346f9192ee3324140b84b
VedanthVDev <vedanthvdev1996@gmail.com>
2023-07-15 18:37:01 +0100

    portfix

--- server_v2.js
+++ server_v2.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const bcrypt = require("bcrypt");
-const PORT = 3002;
+const PORT = 3000;
 
 const { createClient } = require("@supabase/supabase-js");
 
